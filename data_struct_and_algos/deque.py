ecb9bb83f6e2a42c9b359ffd084a7a549cb8a4da
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-06-17 15:45:50 -0700

    using deque and generators

---
+++ data_struct_and_algos/deque.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+from collections import deque
+
+# perform simple text match and yield matching line along with previous N lines
+def search(lines, pattern, history=5):
+    # create a fixed len queue
+    # if a new item is added and the queue is full, the oldest item is automatically removed
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            # instead of using return for a single value
+            # use yield to return an iterator object = an object you can iterate over using next
+            yield line, previous_lines
+        previous_lines.append(line)
+
+# example use on a file
+if __name__ == "__main__":
+    with open('test.txt') as f:
+        # search() will create generator object that can be iterated over
+        # however the code is not run all at once
+        # only calls to next will execute the next step of the code
+        # next is taken care of by the for loop
+        for line, prevlines in search(f, 'python', 2):
+            for pline in prevlines:
+                print(pline, end='')
+            print(line, end='')
+            print('-' * 20)
