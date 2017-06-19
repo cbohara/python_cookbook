b880b8e3d792527630855791289bbdfc0459fd97
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-06-19 13:55:38 -0700

    for readability create slice objects instead of just using indices

---
+++ data_struct_and_algos/naming_slices.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3
+
+# name slices for readability 
+# slice() creates a slice object that can be applied to any object a slice is allowed
+items = [2, 4, 6, 8, 1, 3, 5, 7]
+evens = slice(0, 4)
+print(items[evens])
+odds = slice(4, 8)
+print(items[odds])
+del items[evens]
+print(items)
+
+# you can look at slice object attributes
+a = slice(10, 50, 2)
+print(a.start)
+print(a.stop)
+print(a.step)
